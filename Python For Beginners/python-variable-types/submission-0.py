5e30cdcf1eacb1b5d57dc17772a7fb6c828a46a8
Tatsat454 <78279477+Tatsat454@users.noreply.github.com>
2026-09-21 13:07:16 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+
+integer_type = int()
+float_type = float()
+bool_type = bool()
+string_type = ""
+list_type = []
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
