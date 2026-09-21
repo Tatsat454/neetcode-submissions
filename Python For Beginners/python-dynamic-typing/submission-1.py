e05216218c7bda5679ccfb7f0ff7d62e37783b27
Tatsat454 <78279477+Tatsat454@users.noreply.github.com>
2026-09-21 13:08:53 -0400

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = int()
+print(type(variable))
+variable = float()
+print(type(variable))
+variable = bool()
+print(type(variable))
+variable = ""
+print(type(variable))
+variable = []
+print(type(variable))
